e6d25b1c1a3cc2164cd7d856d79a45c3121c237b
yudonglu1136 <luyudong1136@gmail.com>
2022-12-02 00:03:41 +0300

    sensors

---
+++ controls.js
@@ -0,0 +1,50 @@
+class Controls{
+	constructor(){
+		this.forward = false;
+		this.right = false;
+		this.left = false;
+		this.reverse = false;
+
+		this.#addKeyBoardListeners();
+	}
+
+	#addKeyBoardListeners(){
+		document.onkeydown = (event) =>{
+			switch(event.key){
+				case "ArrowLeft":
+					this.left = true;
+					break;
+				case "ArrowRight":
+					this.right = true;
+					break;
+				case "ArrowUp":
+					this.forward = true;
+					break;
+				case "ArrowDown":
+					this.reverse = true;
+					break;
+			}
+			
+		}
+
+		document.onkeyup = (event) =>{
+			switch(event.key){
+				case "ArrowLeft":
+					this.left = false;
+					break;
+				case "ArrowRight":
+					this.right = false;
+					break;
+				case "ArrowUp":
+					this.forward = false;
+					break;
+				case "ArrowDown":
+					this.reverse = false;
+					break;
+			}
+			
+		}
+	}
+
+
+}
--- main.js
+++ main.js
@@ -1,9 +1,20 @@
 
 const canvas = document.getElementById('myCanvas');
-
-canvas.height = window.innerHeight;
 canvas.width = 200;
-
 const context = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
-car.draw(context);
+const road = new Road(canvas.width/2, canvas.width*0.9);
+const car = new Car(road.getLaneCenter(1), 550, 30, 50);
+
+animate();
+
+function animate(){
+	car.update(road.borders);
+	canvas.height = window.innerHeight;
+
+	context.save();
+	context.translate(0, -car.y+canvas.height*0.7);
+	road.draw(context);
+	car.draw(context);
+	context.restore();
+	requestAnimationFrame(animate);
+}
