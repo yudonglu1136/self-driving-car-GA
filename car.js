623a3d3eab293621de4935d93cf1c5bd409c5492
yudonglu1136 <luyudong1136@gmail.com>
2022-12-10 11:31:44 +0300

    neural network

--- car.js
+++ car.js
@@ -12,9 +12,12 @@ class Car{
 		this.polygon = []; 
 		this.dameged = false;
 		this.carColor = controlType === "KEYS" ? 'black' : 'red';
-
+		this.useBrain = controlType == "AI";
 		if(controlType != "DUMMY"){
 			this.sensor = new Sensor(this);
+			this.brain = new NeuralNetwork(
+				[this.sensor.rayCount, 12, 6, 4]
+			);
 		}
 		
 		this.controls = new Controls(controlType);
@@ -27,6 +30,18 @@ class Car{
 		}
 		if(this.sensor){
 			this.sensor.update(roadBorders, traffic);
+			const offsets = this.sensor.readings.map(
+				s=>s==null? 0 : 1-s.offset 
+			);
+
+			const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+			if(this.useBrain){
+				this.controls.forward = outputs[0];
+				this.controls.left = outputs[1];
+				this.controls.right = outputs[2];
+				this.controls.reverse = outputs[3];
+			}
 		}
 		
 
@@ -115,12 +130,12 @@ class Car{
 
 	}
 
-	draw(ctx){
+	draw(ctx, color, drawSensor=false){
 		if (this.dameged){
 			ctx.fillStyle = "gray";
 		}
 		else{
-			ctx.fillStyle = this.carColor;
+			ctx.fillStyle = color;
 		}
 		ctx.beginPath();
 		ctx.moveTo(this.polygon[0].x, this.polygon[0].y);
@@ -130,7 +145,7 @@ class Car{
 		}
 		
 		ctx.fill();
-		if(this.sensor){
+		if(this.sensor && drawSensor){
 			this.sensor.draw(ctx);
 		}
 		
